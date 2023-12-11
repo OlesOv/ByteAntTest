f030c17676c36d6db1126a89836e9fd138980b3c
OlesOv <oles.ovcharenko@gmail.com>
2023-12-11 10:33:34 +0200

    Switch from Razor Pages to MVC

---
+++ ByteAntTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ByteAntTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
